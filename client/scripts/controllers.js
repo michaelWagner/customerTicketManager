bccf30f4b88ce4a224ca6919aca7c6eea9bdb987
Michael Wagner <wagnerdmike@gmail.com>
2017-09-03 22:12:26 -0700

    Update notes, update menu and colors.

--- client/scripts/controllers.js
+++ client/scripts/controllers.js
@@ -177,21 +177,30 @@ function($scope, $location, $rootScope, $routeParams, TicketingService, Authenti
 ]);;myAppC.controller('TicketDetailsController', ['$scope', '$location', '$rootScope', '$routeParams', 'store',  'TicketingService','AuthenticationService', 'ErrorHandlingService',
 function($scope, $location, $rootScope, $routeParams, store,  TicketingService, AuthenticationService, ErrorHandlingService) {
 
-    var id = ($routeParams.id) 
+    $scope.rep_name = 'Manny';
+    $scope.note = '';
+    $scope.ticket = $routeParams.id.toString();
+    $scope.text = '';
+    $scope.rep = '1';
+    $scope.date = moment().format("YYYY-MM-DDThh:mm:ss.882824Z");
 
     $scope.getTicketDetail = function(){
         var localTickets = (typeof($scope.summary) !== 'undefined') ? $scope.summary : '';
-        var ticketsPromise = TicketingService.GetTicketDetail(id);
+        var ticketsPromise = TicketingService.GetTicketDetail($scope.ticket);
         ticketsPromise.then(
             function(payload) { 
                 $scope.ticketDetail =  payload;
-                $scope.ticketDetail.repName = TicketingService.GetRep($scope.ticketDetail.rep);
                 console.log($scope.ticketDetail)
                 $scope.status = 'success';
             },
             function(error) {
                 $scope.status = 'error';
-                $scope.message = ErrorHandlingService.getMessage(error);
+                var message = ErrorHandlingService.getMessage(error);
+                if(_.isArray(message)){
+                    alert(_.values(message));
+                } else {
+                    alert(message);
+                }
             }
         ); 
     }
@@ -199,10 +208,55 @@ function($scope, $location, $rootScope, $routeParams, store,  TicketingService, 
     $scope.newTicket = function(){
         window.location.href = '/new';
     }
+    $scope.getNotes = function(){
+         var notesPromise = TicketingService.GetNotes($scope.ticket);
+         notesPromise.then(
+            function(payload) { 
+                $scope.ticketDetail.notes =  payload;
+                console.log($scope.notes)
+                $scope.status = 'success';
+            },
+            function(error) {
+                $scope.status = 'error';
+                var message = ErrorHandlingService.getMessage(error);
+                if(_.isArray(message)){
+                    alert(_.values(message));
+                } else {
+                    alert(message);
+                }
+            }
+        ); 
+    }
+    $scope.submitNote = function(){
+        var data = {};
+        data.rep_name = $scope.rep_name;
+        data.ticket   = $scope.ticket;
+        data.rep      = $scope.rep ;
+        data.date     = $scope.date;
+        data.text     = $scope.note;
+    
+        var newNotePromise = TicketingService.PostNote(data);
+        newNotePromise.then(
+            function(payload) { 
+                $scope.newNote =  payload;
+                alert('Ticket Submitted');
+            },
+            function(error) {
+                $scope.status = 'error';
+                var message = ErrorHandlingService.getMessage(error);
+                if(_.isArray(message)){
+                    alert(_.values(message));
+                } else {
+                    alert(message);
+                }
+            }
+        );        
+    }
 
     $scope.logout = function(){
         AuthenticationService.Logout();
     }
+
     $scope.searchTickets = function(item){
         if(item){
             var tickets = store.get('tickets');
@@ -220,6 +274,7 @@ function($scope, $location, $rootScope, $routeParams, store,  TicketingService, 
     }
 
     $scope.getTicketDetail();
+    $scope.getNotes($scope.ticket);
 }
 ]);;
 myAppC.controller('TicketReportsController', ['$scope', '$location', '$rootScope', '$routeParams',  'AuthenticationService', 'TicketingService', 'ErrorHandlingService',
--- clientSrc/js/services/s-ticketing.js
+++ clientSrc/js/services/s-ticketing.js
@@ -213,6 +213,88 @@
                     );
                 });
             },
+            GetNotes : function(id){
+                return $q(function(resolve, reject) {
+                    var token = store.get('token');
+                    return $http({
+                        url:      'https://yellow-jackal-backend.herokuapp.com/api/tickets/' + id + '/notes/',
+                        dataType: 'json',
+                        method:   'GET',
+                        headers:  {
+                            "Content-Type": "application/json",
+                            "Accept":"application/json",
+                            "Authorization": "JWT " + token.token 
+                        },
+                        data:null,
+                        }).then(function(response) {
+                            if (response.status === 200) {
+                                if(response.data.results) {
+                                    resolve( response.data.results);
+                                } else {
+                                    // invalid response
+                                    reject({'status':500,'message':'GetCustomers service request response data is null'});
+                                }
+                            } else {
+                                // invalid response
+                                reject({'status':500,'message':'GetCustomers service request response data is not an object'});
+                            }
+
+                        }, function(err) {
+                            // something went wrong
+                            if(err.status===400){
+                                reject({'status':err.status,'message':err.data});                             
+                            } else if(err.status ===401){
+                                $location.path('/login');
+                            } else if(err.status ===404){
+                                reject({'status':err.status,'message':'file not found'});                             
+                            } else {
+                                reject({'status':err.status,'message':'unknown error'});                             
+                            }
+                    });
+                });
+            },
+            PostNote : function(newNote){
+                return $q(function(resolve, reject){
+                    var token = store.get('token');                    
+                    return $http({
+                        url: 'https://yellow-jackal-backend.herokuapp.com/api/tickets/' + newNote.ticket + '/notes/',
+                        dataType: 'json',
+                        method: 'POST',
+                        data: newNote,
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Accept":"application/json",
+                            "Authorization": "JWT " + token.token 
+                        }
+                    }).then(function(response) {
+                            if (response.status === 201) {
+                                if(response.data != null) {
+                                    console.log(response.data);
+                                    store.set('token', response.data);
+                                    resolve(true);
+                                } else {
+                                    // invalid response
+                                    reject({'status':'error','message':'Ticketing service received a null response'});
+                                }
+                            } else {
+                                // invalid response
+                                reject({'status':'error','message':'Ticketing service received invalid data'});
+                            }
+                        }, function(err) {
+                            // something went wrong
+                            if(err.status===400){
+                                reject({'status':err.status,'message':err.data});                             
+                            } else if(err.status ===401){
+                                reject({'status':err.status,'message':err.data});                             
+                            } else if(err.status ===404){
+                                reject({'status':err.status,'message':'file not found'});                             
+                            } else {
+                                reject({'status':err.status,'message':'unknown authentication error'});                             
+                            }
+                        }
+                    );
+                });
+            },
         }
     }
 ]);
